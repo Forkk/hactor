74ee6952a1a17ce5e608c2c1af7186469cbbefaf
Forkk <forkk@forkk.net>
2014-08-03 23:57:22 -0500

    Enforce type safety for actors

--- Control/Concurrent/Actor.hs
+++ Control/Concurrent/Actor.hs
@@ -7,62 +7,15 @@
 -- Portability :  GHC only (requires throwTo)
 --
 -- This module implements Erlang-style actors
--- (what Erlang calls processes). It does not implement
--- network distribution (yet?). Here is an example:
+-- (what Erlang calls processes).
 --
 -- @
---act1 :: Actor
---act1 = do
---    me <- self
---    liftIO $ print "act1 started"
---    forever $ receive
---      [ Case $ \((n, a) :: (Int, Address)) ->
---            if n > 10000
---                then do
---                    liftIO . throwIO $ NonTermination
---                else do
---                    liftIO . putStrLn $ "act1 got " ++ (show n) ++ " from " ++ (show a)
---                    send a (n+1, me)
---      , Case $ \(e :: RemoteException) ->
---            liftIO . print $ "act1 received a remote exception"
---      , Default $ liftIO . print $ "act1: received a malformed message"
---      ]
---
---act2 :: Address -> Actor
---act2 addr = do
---    monitor addr
---    -- setFlag TrapRemoteExceptions
---    me <- self
---    send addr (0 :: Int, me)
---    forever $ receive
---      [ Case $ \((n, a) :: (Int, Address)) -> do
---                    liftIO . putStrLn $ "act2 got " ++ (show n) ++ " from " ++ (show a)
---                    send a (n+1, me)
---      , Case $ \(e :: RemoteException) ->
---            liftIO . print $ "act2 received a remote exception: " ++ (show e)
---      ]
---
---act3 :: Address -> Actor
---act3 addr = do
---    monitor addr
---    setFlag TrapRemoteExceptions
---    forever $ receive
---      [ Case $ \(e :: RemoteException) ->
---            liftIO . print $ "act3 received a remote exception: " ++ (show e)
---      ]
---
---main = do
---    addr1 <- spawn act1
---    addr2 <- spawn (act2 addr1)
---    spawn (act3 addr2)
---    threadDelay 20000000
--- @
-{-# LANGUAGE DeriveDataTypeable #-}
-{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE TypeSynonymInstances, FlexibleInstances, DeriveDataTypeable #-}
+{-# LANGUAGE MultiParamTypeClasses, AllowAmbiguousTypes, ScopedTypeVariables #-}
+{-# LANGUAGE OverlappingInstances, UndecidableInstances #-}
 module Control.Concurrent.Actor (
   -- * Types
-    Address
-  , Handler(..)
+    ActorHandle
   , ActorM
   , Actor
   , ActorException
@@ -72,7 +25,7 @@ module Control.Concurrent.Actor (
   , send
   , self
   , receive
-  , receiveWithTimeout
+  -- , receiveWithTimeout
   , spawn
   , runActor
   , monitor
@@ -86,67 +39,32 @@ module Control.Concurrent.Actor (
   ) where
 
 import Control.Concurrent
-  ( forkIO
-  , killThread
-  , myThreadId
-  , ThreadId
-  )
 import Control.Exception
-  ( Exception(..)
-  , SomeException
-  , catches
-  , throwTo
-  , throwIO
-  , PatternMatchFail(..)
-  )
 import qualified Control.Exception as E (Handler(..))
-import Control.Concurrent.Chan
-  ( Chan
-  , newChan
-  , readChan
-  , writeChan
-  )
-import Control.Concurrent.MVar
-  ( MVar
-  , newMVar
-  , modifyMVar_
-  , withMVar
-  , readMVar
-  )
 import GHC.Conc (ThreadStatus, threadStatus)
 import Control.Monad.Reader
-  ( ReaderT
-  , runReaderT
-  , asks
-  , ask
-  , liftIO
-  )
-import System.Timeout (timeout)
-import Data.Dynamic
-import Data.Set
-  ( Set
-  , empty
-  , insert
-  , delete
-  , elems
-  )
+import qualified Data.Set as S
+import Data.Typeable
 import Data.Word (Word64)
-import Data.Bits (
-    setBit
-  , clearBit
-  , complementBit
-  , testBit
-  )
+import Data.Bits
+
+-- {{{ Types
+
+-- {{{ Exceptions
 
 -- | Exception raised by an actor on exit
-data ActorExit = ActorExit deriving (Typeable, Show)
+data ActorExit = ActorExit deriving (Show, Typeable)
 
 instance Exception ActorExit
 
-data ActorException = ActorException Address SomeException
-  deriving (Typeable, Show)
+data ActorException msg = ActorException (ActorHandle msg) SomeException
+  deriving (Show, Typeable)
 
-instance Exception ActorException
+instance (ActorMessage msg) => Exception (ActorException msg)
+
+-- }}}
+
+-- {{{ Flags
 
 type Flags = Word64
 
@@ -168,99 +86,128 @@ toggleF = flip complementBit . fromEnum
 isSetF :: Flag -> Flags -> Bool
 isSetF = flip testBit . fromEnum
 
-data Context = Context
-  { ctxMonitors :: MVar (Set Address)
-  , ctxChan     :: Chan Message
+-- }}}
+
+-- {{{ Actor handle and context
+
+-- | The actor context. This holds information such as the actor's links,
+-- channels, and flags.
+data (ActorMessage msg) => Context msg = Context
+  { ctxLinks    :: MVar (S.Set (ActorHandle msg))
+  , ctxChan     :: Chan msg
   , ctxFlags    :: MVar Flags
-  } deriving (Typeable)
+  }
 
-newtype Message = Msg { unMsg :: Dynamic }
-    deriving (Typeable)
+-- | An actor handle.
+-- This is used as a "reference" to an actor. It contains the actor's thread ID
+-- and context.
+data (ActorMessage msg) => ActorHandle msg = ActorHandle
+  { ahThread  :: ThreadId
+  , ahContext :: Context msg
+  }
 
-instance Show Message where
-    show = show . unMsg
+instance (ActorMessage msg) => Show (ActorHandle msg) where
+    show (ActorHandle tid _) = "ActorHandle(" ++ (show tid) ++ ")"
 
-toMsg :: Typeable a => a -> Message
-toMsg = Msg . toDyn
+instance (ActorMessage msg) => Eq (ActorHandle msg) where
+    h1 == h2 = (ahThread h1) == (ahThread h2)
 
-fromMsg :: Typeable a => Message -> Maybe a
-fromMsg = fromDynamic . unMsg
+instance (ActorMessage msg) => Ord (ActorHandle msg) where
+    h1 `compare` h2 = (ahThread h1) `compare` (ahThread h2)
 
--- | The address of an actor, used to send messages
-data Address = Address
-  { addrThread  :: ThreadId
-  , addrContext :: Context
-  } deriving (Typeable)
+-- }}}
 
-instance Show Address where
-    show (Address ti _) = "Address(" ++ (show ti) ++ ")"
-
-instance Eq Address where
-    addr1 == addr2 = (addrThread addr1) == (addrThread addr2)
-
-instance Ord Address where
-    addr1 `compare` addr2 = (addrThread addr1) `compare` (addrThread addr2)
+-- {{{ Actor monad
 
 -- | The actor monad, just a reader monad on top of 'IO'.
-type ActorM = ReaderT Context IO
+type ActorM msg = ReaderT (Context msg) IO
 
--- | The type of an actor. It is just a monadic action
--- in the 'ActorM' monad, returning ()
-type Actor = ActorM ()
+-- | The type of an actor taking the given type of message.
+type Actor msg = ActorM msg ()
 
-data Handler = forall m . (Typeable m)
-             => Case (m -> ActorM ())
-             |  Default (ActorM ())
+-- | Like MonadIO, but for the actor monad.
+class (Monad m, ActorMessage msg) => MonadActor msg m where
+    liftActor :: ActorM msg a -> m a
+
+instance (ActorMessage msg) => MonadActor msg (ActorM msg) where
+    liftActor = id
+
+-- }}}
+
+-- {{{ Messages
+
+class (Show msg, Typeable msg) => ActorMessage msg where
+
+-- }}}
+
+-- }}}
+
+-- {{{ Utility functions
+
+-- | Gets the channel for the given actor handle.
+ahChan :: (ActorMessage msg) => ActorHandle msg -> Chan msg
+ahChan = ctxChan . ahContext
+
+-- }}}
+
+-- {{{ Actor functions
+
+-- {{{ Information & management
+
+-- | Kill the actor at the specified address
+kill :: (ActorMessage msg) => ActorHandle msg -> ActorM msg ()
+kill = liftIO . killThread . ahThread
+
+-- | The current status of an actor
+status :: (ActorMessage msg) => ActorHandle msg -> ActorM msg ThreadStatus
+status = liftIO . threadStatus . ahThread
 
 -- | Used to obtain an actor's own address inside the actor
-self :: ActorM Address
+self :: (ActorMessage msg) => ActorM msg (ActorHandle msg)
 self = do
     c <- ask
     i <- liftIO myThreadId
-    return $ Address i c
+    return $ ActorHandle i c
+
+-- }}}
+
+-- {{{ Messages
 
 -- | Try to handle a message using a list of handlers.
 -- The first handler matching the type of the message
 -- is used.
-receive :: [Handler] -> ActorM ()
-receive hs = do
-    ch  <- asks ctxChan
-    msg <- liftIO . readChan $ ch
-    rec msg hs
+receive :: (MonadIO m, MonadActor msg m, ActorMessage msg) => (msg -> m a) -> m a
+receive handler =
+    handler =<< (liftIO . readChan) =<< (liftActor $ asks ctxChan)
 
--- | Same as receive, but times out after a specified
+-- TODO: Re-implement receiveWithTimeout
+-- Same as receive, but times out after a specified
 -- amount of time and runs a default action
-receiveWithTimeout :: Int -> [Handler] -> ActorM () -> ActorM ()
-receiveWithTimeout n hs act = do
-    ch <- asks ctxChan
-    msg <- liftIO . timeout n . readChan $ ch
-    case msg of
-        Just m  -> rec m hs
-        Nothing -> act
+-- receiveWithTimeout :: Int -> [Handler] -> ActorM () -> ActorM ()
+-- receiveWithTimeout n hs act = do
+--     ch <- asks ctxChan
+--     msg <- liftIO . timeout n . readChan $ ch
+--     case msg of
+--         Just m  -> rec m hs
+--         Nothing -> act
 
-rec :: Message -> [Handler] -> ActorM ()
-rec msg [] = liftIO . throwIO $ PatternMatchFail err where
-    err = "no handler for messages of type " ++ (show msg)
-rec msg ((Case hdl):hs) = case fromMsg msg of
-    Just m  -> hdl m
-    Nothing -> rec msg hs
-rec _ ((Default act):_) = act
+-- | Sends a message to the given actor.
+send :: (MonadIO m, MonadActor msg m, ActorMessage msg, ActorMessage smsg) =>
+        ActorHandle smsg -> smsg -> m ()
+send h = liftIO . writeChan (ahChan h)
 
+-- }}}
 
--- | Sends a message from inside the 'ActorM' monad
-send :: Typeable m => Address -> m -> ActorM ()
-send addr msg = do
-    let ch = ctxChan . addrContext $ addr
-    liftIO . writeChan ch . toMsg $ msg
+-- {{{ Start actors
 
 -- FIXME: This is a big and complicated function. It would be a good idea to
 -- break it up into smaller parts.
 -- | Prepares the given actor monad to be run as an actor. Returns a tuple
 -- containing the actor's context and an IO action to run the actor.
-mkActor :: Actor -> [Flag] -> IO (IO (), Context)
+mkActor :: (ActorMessage msg) => Actor msg -> [Flag] -> IO (IO (), Context msg)
 mkActor actor flagList = do
     chan <- liftIO newChan
-    linkedActors <- newMVar empty
+    linkedActors <- newMVar S.empty
     flags <- newMVar $ foldl (flip setF) 0x00 flagList
     let context = Context linkedActors chan flags
     -- An IO action which executes the actual actor.
@@ -271,60 +218,62 @@ mkActor actor flagList = do
         actorInternal = runReaderT actor context
     -- Linked exception handler. Catches exceptions from linked actors and
     -- handles them appropriately.
-        linkedHandler :: ActorException -> IO ()
         linkedHandler ex@(ActorException addr iex) = do
             -- Remove the linked actor from our list.
-            modifyMVar_ linkedActors (return . delete addr)
+            modifyMVar_ linkedActors (return . S.delete addr)
             me <- myThreadId
-            forward $ ActorException (Address me context) iex
+            forward $ ActorException (ActorHandle me context) iex
             throwIO ex
     -- Exception handler. Catches all exceptions and forwards them to
     -- monitoring actors.
         exceptionHandler :: SomeException -> IO ()
         exceptionHandler ex = do
             me <- myThreadId
-            forward $ ActorException (Address me context) ex
+            forward $ ActorException (ActorHandle me context) ex
             throwIO ex
     -- Exception forwarding function.
     -- Takes an `ActorException` and forwards it to all actors monitoring the
     -- current actor.
-        forward :: ActorException -> IO ()
         forward ex = do
             linkedSet <- readMVar linkedActors
-            mapM_ (forwardTo ex) $ elems linkedSet
+            mapM_ (forwardTo ex) $ S.elems linkedSet
     -- Forwards the given exception to the given actor.
-        forwardTo :: ActorException -> Address -> IO ()
         forwardTo ex addr = do
-            let remoteFlags = ctxFlags . addrContext $ addr
-                remoteChan  = ctxChan  . addrContext $ addr
-            trap <- withMVar remoteFlags (return . isSetF TrapActorExceptions)
+            -- let remoteFlags = ctxFlags . ahContext $ addr
+            --     remoteChan  = ctxChan  . ahContext $ addr
+            throwTo (ahThread addr) ex
+            -- TODO: Re-implement trapping exceptions
+            -- trap <- withMVar remoteFlags (return . isSetF TrapActorExceptions)
             -- If the remote actor is trapping actor exceptions, send our
             -- exception to it as a message. Otherwise, throw it to the actor's
             -- thread.
-            if trap
-               then writeChan remoteChan $ toMsg ex
-               else throwTo (addrThread addr) ex
+            -- if trap
+            --    then writeChan remoteChan $ ex
+            --    else throwTo (ahThread addr) ex
     -- Return the context and the IO action.
     return (actorFunc, context)
 
 
 -- | Spawn a new actor with default flags on a separate thread.
-spawn :: Actor -> IO Address
+spawn :: (ActorMessage msg) => Actor msg -> IO (ActorHandle msg)
 spawn actor = do
     (actorFunc, context) <- mkActor actor defaultFlags
     -- Start the actor's thread.
     threadId <- forkIO actorFunc
     -- Return a handle.
-    return $ Address threadId context
+    return $ ActorHandle threadId context
 
 -- | Run the given actor with default flags on the current thread.
 -- This can be useful for your program's "main actor".
-runActor :: Actor -> IO ()
+runActor :: (ActorMessage msg) => Actor msg -> IO ()
 runActor actor = do
     -- Create the actor.
     (actorFunc, _) <- mkActor actor defaultFlags
     actorFunc
 
+-- }}}
+
+-- {{{ Error handling
 
 -- | Monitors the actor at the specified address.
 -- If an exception is raised in the monitored actor's
@@ -332,48 +281,48 @@ runActor actor = do
 -- forwarded to the monitoring actor. If the monitored
 -- actor terminates, an 'ActorException' is raised in
 -- the monitoring Actor
-monitor :: Address -> ActorM ()
+monitor :: (ActorMessage msg) => ActorHandle msg -> ActorM msg ()
 monitor addr = do
     me <- self
-    let mons = ctxMonitors . addrContext $ addr
-    liftIO $ modifyMVar_ mons (return . insert me)
+    let mons = ctxLinks . ahContext $ addr
+    liftIO $ modifyMVar_ mons (return . S.insert me)
 
 -- | Like `monitor`, but bi-directional
-link :: Address -> ActorM ()
+link :: (ActorMessage msg) => ActorHandle msg -> ActorM msg ()
 link addr = do
     monitor addr
-    mons <- asks ctxMonitors
-    liftIO $ modifyMVar_ mons (return . insert addr)
+    mons <- asks ctxLinks
+    liftIO $ modifyMVar_ mons (return . S.insert addr)
 
--- | Kill the actor at the specified address
-kill :: Address -> ActorM ()
-kill = liftIO . killThread . addrThread
+-- }}}
 
--- | The current status of an actor
-status :: Address -> ActorM ThreadStatus
-status = liftIO . threadStatus . addrThread
+-- {{{ Flags
 
 -- | Sets the specified flag in the actor's environment
-setFlag :: Flag -> ActorM ()
+setFlag :: (ActorMessage msg) => Flag -> ActorM msg ()
 setFlag flag = do
     fs <- asks ctxFlags
     liftIO $ modifyMVar_ fs (return . setF flag)
 
 -- | Clears the specified flag in the actor's environment
-clearFlag :: Flag -> ActorM ()
+clearFlag :: (ActorMessage msg) => Flag -> ActorM msg ()
 clearFlag flag = do
     fs <- asks ctxFlags
     liftIO $ modifyMVar_ fs (return . clearF flag)
 
 -- | Toggles the specified flag in the actor's environment
-toggleFlag :: Flag -> ActorM ()
+toggleFlag :: (ActorMessage msg) => Flag -> ActorM msg ()
 toggleFlag flag = do
     fs <- asks ctxFlags
     liftIO $ modifyMVar_ fs (return . toggleF flag)
 
 -- | Checks if the specified flag is set in the actor's environment
-testFlag :: Flag -> ActorM Bool
+testFlag :: (ActorMessage msg) => Flag -> ActorM msg Bool
 testFlag flag = do
     fs <- asks ctxFlags
     liftIO $ withMVar fs (return . isSetF flag)
+
+-- }}}
+
+-- }}}
 
